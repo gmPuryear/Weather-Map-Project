196f2734ff1edc246506c87062a5bd498aa04db6
George Puryear <gm.puryear@yahoo.com>
2022-04-17 09:57:30 -0500

    all files for weather-project

---
+++ js/keys.js
@@ -0,0 +1,5 @@
+const OWM_KEY = "74f2096fa0dd49a40def83a89e6f7eb2";
+const WEATHER = "https://api.openweathermap.org/data/2.5/weather?lat=30.5&lon=-97.7&units=imperial&appid=74f2096fa0dd49a40def83a89e6f7eb2";
+const MAPBOX = "pk.eyJ1IjoiZ3B1cnllYXIiLCJhIjoiY2wwdXpuenlpMHN3djNicDc4Z2t1bWx1dCJ9.HI6r2Ig6Z59kSRcM3bcYtA";
+const DICTIONARY = "https://dictionaryapi.com/api/v3/references/collegiate/json/test?key=57e06377-5347-4313-8de6-ee57f9759b42";
+const GITHUB = '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';
